53ca942e95a3a48392bbcb3bb572354b11f09471
tancredo fernando <tancredofernandoneves@gmail.com>
2017-03-22 02:13:10 -0300

    Ajustes Module

---
+++ Filmoteca/AngularApp/js/Module.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module("filmoteca", ['ngMask', 'ngRoute']);
+
+app.config(function ($routeProvider, $locationProvider) {
+
+    $routeProvider
+        .when("/", { templateUrl: "/cliente/html/lista.html", controller: "listaController" })
+        .when("/detalhes/:id", { templateUrl: "/cliente/html/detalhes.html", controller: "detalhesController" })
+        .otherwise({ redirecTo: "/" });
+
+
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    app.constant("filmeApiUrl", "/api/filme/");
+});
